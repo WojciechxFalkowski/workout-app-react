054339d58c930a0076b73bc91a0e5df7fd68bab9
Wojciech Falkowski <wojciechxfalkowski@gmail.com>
2020-10-13 18:26:16 +0200

    update Exercise component

--- src/pages/Exercise/Exercise.tsx
+++ src/pages/Exercise/Exercise.tsx
@@ -1,15 +1,18 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { useHistory } from "react-router-dom";
 import { Arrow } from "./../Training/components";
 import { AuthContext } from "components/AuthProvider/AuthProvider";
 import { FormInput } from "./components";
 import { required, composeValidators } from "utils/validation";
+import fire from "./../../fire";
 import "./exercise.scss";
 interface Id {
   id: string;
 }
 interface Params {
   params: Id;
+  path: string;
+  url: string;
 }
 export interface Props {
   match: Params;
@@ -19,6 +22,8 @@ const Exercise: React.FC<Props> = (props) => {
   let history = useHistory();
   const { currentUser } = useContext(AuthContext);
   const { exerciseName }: any = history.location.state;
+  const url = props.match.url.replace("/trainings/", "");
+  const id = url.replace("/" + props.match.params.id, "");
   const [formFields, setFormFields] = useState({
     fields: [
       {
@@ -41,48 +46,72 @@ const Exercise: React.FC<Props> = (props) => {
       text: "Zapisz",
     },
   });
-
-  const checkSeries = (series: Array<string>) => {
-    return series.map((item, index) => {
-      const firstHalf = Number(item.substring(0, item.indexOf(":")));
-      const secondHalf = Number(
-        item.substring(item.indexOf(":") + 1, item.length)
-      );
-      // console.log(typeof firstHalf);
-      // console.log(`element: ${index} ${firstHalf},${secondHalf}`);
-      // console.log(`tablica typ: ${typeof series}`);
-      // console.log(`tablica: ${series}`);
-      // console.log(`tablica długość: ${series.length}`);
-    });
-  };
-
-  const handleDeleteExercise = () => {
+  const handleSaveExercise = () => {
     if (currentUser) {
-      // fire
-      //   .database()
-      //   .ref("users/" + currentUser.uid + "/trainings/")
-      //   .remove();
+      fire
+        .database()
+        .ref(
+          `users/${currentUser.uid}/trainings/${id}/exercises/${props.match.params.id}`
+        )
+        .remove();
       history.goBack();
     }
   };
   const handleSubmit = (values: any) => {
-    // console.log("handleSubmit");
-    // console.log("ilosc pól:", formFields.fields.length);
-    // console.log("pola:", formFields.fields);
-    formFields.fields.map((field) => {
-      console.log(values[field.name]);
+    const newArray: any = [];
+    formFields.fields.forEach((field) => {
+      newArray.push(values[field.name]);
     });
-    // console.log(values);
+    if (currentUser) {
+      const url = `users/${currentUser.uid}/trainings/${id}/exercises/${props.match.params.id}`;
+      fire.database().ref(url).child("series").set(newArray);
+    }
   };
-
-  const handleSaveExercise = () => {
-    console.log("handleSaveExercise");
+  const ID = function () {
+    return (
+      Math.random().toString(36).substr(2, 9) +
+      Math.random().toString(36).substr(2, 9)
+    );
   };
-
+  useEffect(() => {
+    if (currentUser) {
+      fire
+        .database()
+        .ref(
+          `users/${currentUser.uid}/trainings/${id}/exercises/${props.match.params.id}/series`
+        )
+        .on("value", function (snapshot) {
+          if (snapshot) {
+            const fields: any = [];
+            const button = {
+              type: "submit",
+              text: "Zapisz",
+            };
+            snapshot.forEach(function (childSnapshot) {
+              fields.push({
+                name: `${
+                  fields.length % 2 === 0 ? "exerciseWeight" : "exerciseRepeat"
+                }${ID()}`,
+                validate: composeValidators(required("To pole jest wymagane!")),
+                initialValue: childSnapshot.val(),
+                text: "Ciężar",
+                placeholder: "Ciężar",
+              });
+            });
+            if (fields.length > 0) {
+              setFormFields({
+                fields,
+                button,
+              });
+            }
+          }
+        });
+    }
+  }, [currentUser, id, props.match.params.id]);
   return (
     <div className="exercise">
       <Arrow />
-      <button onClick={handleDeleteExercise} className="exercise__button">
+      <button onClick={handleSaveExercise} className="exercise__button">
         Usuń ćwiczenie
       </button>
       <h2 className="exercise__h2">{exerciseName}</h2>
@@ -91,12 +120,6 @@ const Exercise: React.FC<Props> = (props) => {
         setFormFields={setFormFields}
         handleSubmit={handleSubmit}
       />
-      {/* <div className="exercise__add">
-        <AiOutlineCheck
-          onClick={handleSaveExercise}
-          className="exercise__icon"
-        />
-      </div> */}
     </div>
   );
 };
