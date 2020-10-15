71b176bde9f640124554c4449355ebffdaece563
Wojciech Falkowski <wojciechxfalkowski@gmail.com>
2020-10-15 18:11:05 +0200

    Updates

--- precache-manifest.27c599c731aa10f060bcce27c0d3113b.js
+++ precache-manifest.27c599c731aa10f060bcce27c0d3113b.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9c5b77e07ff71206bf83c106d8f9c744",
+    "revision": "8f551d8d458523daa6705d9c780c0441",
     "url": "/workout-app-react/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/workout-app-react/static/css/2.117ce29f.chunk.css"
   },
   {
-    "revision": "28c607d6cafe7a4d48dd",
+    "revision": "7d0a2bbbbd02ee82b101",
     "url": "/workout-app-react/static/css/main.c39007f4.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/workout-app-react/static/js/2.466a7f73.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "28c607d6cafe7a4d48dd",
-    "url": "/workout-app-react/static/js/main.49df9e1b.chunk.js"
+    "revision": "7d0a2bbbbd02ee82b101",
+    "url": "/workout-app-react/static/js/main.f391c8bc.chunk.js"
   },
   {
     "revision": "a8ea7c4f458acf99c3f2",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/workout-app-react/precache-manifest.71f759dc95dd1ab882645f7428677725.js"
+  "/workout-app-react/precache-manifest.27c599c731aa10f060bcce27c0d3113b.js"
 );
 
 self.addEventListener('message', (event) => {
