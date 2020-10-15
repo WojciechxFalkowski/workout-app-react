8b62de434b0c8b0f69a4f682c2ad36ced9afc993
Wojciech Falkowski <wojciechxfalkowski@gmail.com>
2020-10-15 16:35:42 +0200

    Updates

---
+++ precache-manifest.71f759dc95dd1ab882645f7428677725.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c5b77e07ff71206bf83c106d8f9c744",
+    "url": "/workout-app-react/index.html"
+  },
+  {
+    "revision": "6143a62bb57bafdd51e0",
+    "url": "/workout-app-react/static/css/2.117ce29f.chunk.css"
+  },
+  {
+    "revision": "28c607d6cafe7a4d48dd",
+    "url": "/workout-app-react/static/css/main.c39007f4.chunk.css"
+  },
+  {
+    "revision": "6143a62bb57bafdd51e0",
+    "url": "/workout-app-react/static/js/2.466a7f73.chunk.js"
+  },
+  {
+    "revision": "2415e6967177769184874497a3c934bc",
+    "url": "/workout-app-react/static/js/2.466a7f73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28c607d6cafe7a4d48dd",
+    "url": "/workout-app-react/static/js/main.49df9e1b.chunk.js"
+  },
+  {
+    "revision": "a8ea7c4f458acf99c3f2",
+    "url": "/workout-app-react/static/js/runtime-main.b27fd735.js"
+  }
+]);
